31a0dd03af35d3469b7149d5de02f6beaf5cef03
Olamide <glofad2000@gmail.com>
2021-07-27 14:46:53 +0000

    push

--- app.py
+++ app.py
@@ -2,15 +2,18 @@
 # -- Import section --
 from flask import Flask, render_template, request
 from datetime import datetime
-# from model import getImageUrlFrom
+from model import getClothingPrices
 import os
 
 # -- Initialization section --
 app = Flask(__name__)
 
 
+
 # -- Routes section --
 @app.route('/')
 @app.route('/index')
 def index():
+    getClothingPrices()
     return render_template("index.html", time = datetime.now())
+
