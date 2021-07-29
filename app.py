a32a349ef17dfc13be9c2ff0f910011f584f2bd6
Olamide <glofad2000@gmail.com>
2021-07-29 15:31:56 +0000

    messages

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 # -- Import section --
 from flask import Flask, render_template, request
 from datetime import datetime
-from model import getClothingWomenDetails
+from model import getClothingWomenDetails, getClothingWomen1, getClothingWomen2,getClothingWomen3, getClothingMen1, getClothingMen2,getClothingMen3 
 import os
 
 # -- Initialization section --
@@ -21,4 +21,40 @@ def index():
 
 @app.route('/men')
 def men():
+    
     return render_template("men.html")
+
+# @app.route('/women')
+# def women():
+#     details = getClothingWomenDetails()
+#     return render_template("women.html", details =details)
+
+@app.route('/women$')
+def women1():
+    details = getClothingWomen1()
+    return render_template("women$.html", details =details)
+
+@app.route('/women$$')
+def women2():
+    details = getClothingWomen2()
+    return render_template("women$$.html", details =details)
+
+@app.route('/women$$$')
+def women3():
+    details = getClothingWomen3()
+    return render_template("women$$$.html", details =details)
+
+@app.route('/men$')
+def men1():
+    details = getClothingMen1()
+    return render_template("men$.html", details =details)
+
+@app.route('/men$$')
+def men2():
+    details = getClothingMen2()
+    return render_template("men$$.html", details =details)
+
+@app.route('/men$$$')
+def men3():
+    details = getClothingMen3()
+    return render_template("men$$$.html", details =details)
--- model.py
+++ model.py
@@ -30,11 +30,11 @@ def getClothingWomenDetails():
         details.append(temp_detail)
     return details
 
-
-def getClothingUrl():
+def getClothingMen1():
+    
     url = "https://apidojo-forever21-v1.p.rapidapi.com/products/v2/list"
 
-    querystring = {"category": "men_main"}
+    querystring = {"category": "mens_main"}
 
     # querystring = {"category": "women_main", "pageSize": "48", "pageNumber": "1",
     #             "sortby": "0", "filterColor": "BLACK", "filterSize": "XS/S"}
@@ -53,5 +53,101 @@ def getClothingUrl():
 
     for data in range(datalength):
         temp_data = {"url": response["CatalogProducts"][data]["DefaultProductImage"], "price": response["CatalogProducts"][data]["ListPrice"] , "title" :response["CatalogProducts"][data]["DisplayName"]}
-        data.append(temp_data)
+        if temp_data["price"] < 10 :
+            data.append(temp_data)
     return data
+
+def getClothingWomen1():
+    url = "https://apidojo-forever21-v1.p.rapidapi.com/products/v2/list"
+
+    querystring = {"category": "women_main", "pageSize": "48", "pageNumber": "1",
+                   "sortby": "0"}
+ 
+    headers = {
+        'x-rapidapi-key': "91bcf22dd9msh88ccbae757c3c69p14ee16jsna69aea48484e",
+        'x-rapidapi-host': "apidojo-forever21-v1.p.rapidapi.com"
+    }
+
+    response = requests.request(
+        "GET", url, headers=headers, params=querystring).json()
+
+    # # print(response.text)
+    # print(response["CatalogProducts"][0]["ListPrice"])
+
+    # price = response["CatalogProducts"][0]["ListPrice"]
+    # return price
+
+    detailslength = len(response["CatalogProducts"])
+
+    details = []
+
+    for detail in range(detailslength):
+        temp_detail = {"url": response["CatalogProducts"][detail]["DefaultProductImage"], "price": response["CatalogProducts"][detail]["ListPrice"] , "title" :response["CatalogProducts"][detail]["DisplayName"]}
+        if temp_detail["price"] < 10 :
+           details.append(temp_detail)
+    return details
+
+
+def getClothingWomen2():
+    url = "https://apidojo-forever21-v1.p.rapidapi.com/products/v2/list"
+
+    querystring = {"category": "women_main", "pageSize": "48", "pageNumber": "1",
+                   "sortby": "0"}
+ 
+    headers = {
+        'x-rapidapi-key': "91bcf22dd9msh88ccbae757c3c69p14ee16jsna69aea48484e",
+        'x-rapidapi-host': "apidojo-forever21-v1.p.rapidapi.com"
+    }
+
+    response = requests.request(
+        "GET", url, headers=headers, params=querystring).json()
+
+    # # print(response.text)
+    # print(response["CatalogProducts"][0]["ListPrice"])
+
+    # price = response["CatalogProducts"][0]["ListPrice"]
+    # return price
+
+    detailslength = len(response["CatalogProducts"])
+
+    details = []
+
+    for detail in range(detailslength):
+        temp_detail = {"url": response["CatalogProducts"][detail]["DefaultProductImage"], "price": response["CatalogProducts"][detail]["ListPrice"] , "title" :response["CatalogProducts"][detail]["DisplayName"]}
+        if temp_detail["price"] > 20 and temp_detail["price"] < 30 :
+           details.append(temp_detail)
+    return details
+
+
+def getClothingWomen3():
+    url = "https://apidojo-forever21-v1.p.rapidapi.com/products/v2/list"
+
+    querystring = {"category": "women_main", "pageSize": "48", "pageNumber": "1",
+                   "sortby": "0"}
+ 
+    headers = {
+        'x-rapidapi-key': "91bcf22dd9msh88ccbae757c3c69p14ee16jsna69aea48484e",
+        'x-rapidapi-host': "apidojo-forever21-v1.p.rapidapi.com"
+    }
+
+    response = requests.request(
+        "GET", url, headers=headers, params=querystring).json()
+
+    # # print(response.text)
+    # print(response["CatalogProducts"][0]["ListPrice"])
+
+    # price = response["CatalogProducts"][0]["ListPrice"]
+    # return price
+
+    detailslength = len(response["CatalogProducts"])
+
+    details = []
+
+    for detail in range(detailslength):
+        temp_detail = {"url": response["CatalogProducts"][detail]["DefaultProductImage"], "price": response["CatalogProducts"][detail]["ListPrice"] , "title" :response["CatalogProducts"][detail]["DisplayName"]}
+        if temp_detail["price"] > 30 :
+           details.append(temp_detail)
+    return details
+
+    
+   
